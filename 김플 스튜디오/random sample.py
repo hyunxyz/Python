b32a8b08bcfb160f6f038b40cc9677493a21de6d
hyunxyz <isbll.xyz@gmail.com>
2020-09-14 20:17:55 +0900

    added explanation

--- 김플 스튜디오/random sample.py
+++ 김플 스튜디오/random sample.py
@@ -1,7 +1,6 @@
-import random
+import random   #모듈 가져오기
 
 i = ['a','b','c','d','e','f','g','h']
 print(random.sample(i,4))
 
-print(random.sample(range(1,47),6))
-
+print(random.sample(range(1,47),5))  #로또번호/이벤트 당첨자 추출기
